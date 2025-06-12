e6df320ff3ae7d2b155d3e1ebc22ec2c8ff8d9a4
Fahmi Andika <fahmiandika31@gmail.com>
2025-06-12 18:44:50 +0700

    adjust vacancy permission

--- src/routes/vacancyRoute.js
+++ src/routes/vacancyRoute.js
@@ -4,7 +4,7 @@ const controller = require('../controllers/vacancyController');
 const { authenticate, checkRole } = require('../middleware/authMiddleware');
 
 router.get('/vacancy/', controller.getAll);
-router.post('/vacancy/', authenticate, checkRole('admin'), controller.create);
+router.post('/vacancy/', authenticate, checkRole('admin', 'guru'), controller.create);
 router.get('/vacancy/:id', controller.getOne);
 router.put('/vacancy/:id', authenticate, checkRole('admin'), controller.update);
 router.delete('/vacancy/:id', authenticate, checkRole('admin'), controller.remove);
